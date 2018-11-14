c66d661dde045a34659a4b0cb2cb0abd15249e0e
iceviper1914 <mehiorob@gmail.com>
2018-11-14 14:59:47 -0500

    updates

--- collection/views.py
+++ collection/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # this is your new view
-    return render(request, 'index.html')
+    number = 6
+    return render(request, 'index.html', { 'number': number,})
