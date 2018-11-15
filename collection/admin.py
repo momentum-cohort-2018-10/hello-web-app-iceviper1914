da402a5ae7cddc52ffa624b3c2165fb8f8ded4ad
iceviper1914 <mehiorob@gmail.com>
2018-11-14 21:56:45 -0500

    updates on files

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from collection.models import Meme
+class MemeAdmin(admin.ModelAdmin):
+    model = Meme
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Meme, MemeAdmin)
 # Register your models here.
--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Meme(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+
 # Create your models here.
